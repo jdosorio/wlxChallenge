d8ca6666b9e62ba5dd2a3b1dead726c228f10dd1
Juan Osorio <juan.osorio@optima.net.co>
2021-05-12 16:57:10 -0500

    verify url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://private-8e8921-woloxfrontendinverview.apiary-mock.coma'
+  apiUrl: 'https://private-8e8921-woloxfrontendinverview.apiary-mock.com'
 };
 
 /*
